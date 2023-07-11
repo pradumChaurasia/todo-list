b663e2365f3317dd9695b517f5e7e1c54cca8d1d
Pradum <pradum.19441@gmail.com>
2023-07-11 21:24:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/HandleApi";
+import Header from "./components/Header";
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from "@material-ui/icons/Add";
+
 
 function App() {
+
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+
+    <Header/>
+
+      <div className="container">
+
+     
+
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Add ToDos..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <div
+            className="add"
+            onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? <EditIcon/>  : <AddIcon />}
+          </div>
+
+        </div>
+
+        <div className="list">
+
+          {toDo.map((item) => <ToDo 
+          key={item._id} 
+          text={item.text}
+          updateMode = {() => updateMode(item._id, item.text)}
+          deleteToDo = {() => deleteToDo(item._id, setToDo)} />)}
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
